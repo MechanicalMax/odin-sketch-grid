d42ce521d44ca3d613e6e26e34c8902979b60b17
Maximus Shurr <maxshurr@gmail.com>
2025-04-26 18:46:39 -0400

    Separate Button and Sketch Grid in Article

--- sketch-grid.js
+++ sketch-grid.js
@@ -6,14 +6,22 @@ const sketchGridContainerElement = document.querySelector(sketchGridContainerEle
 function populateSketchGrid(grid, sideLength) {
     for (let i = 0; i < sideLength**2; i++) {
         const square = document.createElement("div");
-        square.className = "square"
+        square.className = "square";
         grid.appendChild(square);
     }
 }
 
-const article = document.createElement("article")
+const article = document.createElement("article");
 article.classList.add("sketch-grid");
 
-populateSketchGrid(article, 16);
+const newGridButton = document.createElement("button");
+newGridButton.type = "button";
+newGridButton.innerText = "New Grid";
 
+const grid = document.createElement("div");
+
+populateSketchGrid(grid, 16);
+
+article.appendChild(newGridButton);
+article.appendChild(grid);
 sketchGridContainerElement.appendChild(article);

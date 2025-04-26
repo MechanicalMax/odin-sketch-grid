a2b9dddeb28852a2472ff4e2b5414b0d8f05ae3f
Maximus Shurr <maxshurr@gmail.com>
2025-04-26 18:40:41 -0400

    Fill Sketch Grid with Divs

--- sketch-grid.js
+++ sketch-grid.js
@@ -3,7 +3,17 @@ const sketchGridContainerElementSelector = "main";
 const sketchGridContainerElement = document.querySelector(sketchGridContainerElementSelector);
 
 // Create Sketch Grid
+function populateSketchGrid(grid, sideLength) {
+    for (let i = 0; i < sideLength**2; i++) {
+        const square = document.createElement("div");
+        square.className = "square"
+        grid.appendChild(square);
+    }
+}
+
 const article = document.createElement("article")
 article.classList.add("sketch-grid");
+
+populateSketchGrid(article, 16);
 
 sketchGridContainerElement.appendChild(article);

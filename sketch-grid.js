90113e7aefbfdcbc572152e3e1e4a576db6ed354
Maximus Shurr <maxshurr@gmail.com>
2025-04-26 18:34:27 -0400

    Insert Sketch Grid article into Container Element by Selector

--- sketch-grid.js
+++ sketch-grid.js
@@ -1,1 +1,9 @@
-console.log("Sketch Grid!");
+// Target the container element for the Sketch Grid
+const sketchGridContainerElementSelector = "main";
+const sketchGridContainerElement = document.querySelector(sketchGridContainerElementSelector);
+
+// Create Sketch Grid
+const article = document.createElement("article")
+article.classList.add("sketch-grid");
+
+sketchGridContainerElement.appendChild(article);
